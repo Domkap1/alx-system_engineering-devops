306b993c7e9445f346dadb21f8a6c1b044fa0c39
Domkap1 <domkappaul@gmail.com>
2024-05-03 22:42:03 +0100

    almost

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,11 @@
 # 1-install_a_package.pp
 
-# Install Flask using pip3
-
-package { 'python3-pip':
+# Ensure Python3 and pip3 are installed
+package { ['python3', 'python3-pip']:
   ensure => installed,
 }
 
+# Install Flask version 2.1.0 using pip3
 exec { 'install_flask':
   command => '/usr/bin/pip3 install Flask==2.1.0',
   unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
