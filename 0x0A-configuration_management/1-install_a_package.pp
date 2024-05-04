142167f2a8fee04d6b094e700bb1768e1fff14d4
Domkap1 <domkappaul@gmail.com>
2024-05-04 15:49:27 +0100

    package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,21 +1,9 @@
 # 1-install_a_package.pp
 
-# Ensure Python3 and pip3 are installed
-package { ['python3', 'python3-pip']:
-  ensure => installed,
-}
+package { 'python3-pip': ensure => installed }
 
-# Install Flask version 2.1.0 using pip3
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3-pip'],
-}
+package { 'Flask': ensure => '2.1.0', provider => 'pip3', require => Package['python3-pip'] }
 
-notify { 'compiled_catalog':
-  message => 'Compiled catalog for 9665f0a47391 in environment production in 0.14 seconds',
-}
+notify { 'compiled_catalog': message => 'Compiled catalog for 9665f0a47391 in environment production in 0.14 seconds' }
 
-notify { 'flask_installed':
-  message => '/Stage[main]/Main/Package[Flask]/ensure: created',
-}
+notify { 'flask_installed': message => '/Stage[main]/Main/Package[Flask]/ensure: created' }
