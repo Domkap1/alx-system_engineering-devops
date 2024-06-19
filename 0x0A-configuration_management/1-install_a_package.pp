c06a04ded316899931b7b9831db4b9c5ecb672ea
Domkap1 <111006017+Domkap1@users.noreply.github.com>
2024-06-19 20:53:27 +0100

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,9 +0,0 @@
-# 1-install_a_package.pp
-
-package { 'python3-pip': ensure => installed }
-
-package { 'Flask': ensure => '2.1.0', provider => 'pip3', require => Package['python3-pip'] }
-
-notify { 'compiled_catalog': message => 'Compiled catalog for 9665f0a47391 in environment production in 0.14 seconds' }
-
-notify { 'flask_installed': message => '/Stage[main]/Main/Package[Flask]/ensure: created' }
