2571b776fd58ee5bc370e099f054ccd460d0be9d
Domkap1 <domkappaul@gmail.com>
2024-08-14 07:42:08 -0700

    config mgt

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,14 @@
-# 1-install_a_package.pp
+# Puppet manifest to install Flask version 2.1.0 using pip3
 
-package { 'python3-pip': ensure => installed }
+# Ensure pip3 is installed
+package { 'python3-pip':
+  ensure => installed,
+}
 
-package { 'Flask': ensure => '2.1.0', provider => 'pip3', require => Package['python3-pip'] }
-
-notify { 'compiled_catalog': message => 'Compiled catalog for 9665f0a47391 in environment production in 0.14 seconds' }
-
-notify { 'flask_installed': message => '/Stage[main]/Main/Package[Flask]/ensure: created' }
-
+# Install Flask version 2.1.0 using pip3
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => pip3,
+  require  => Package['python3-pip'],
+}
 
